31b15c5e027b590d7226252c36371049543a053e
majestronik <frknkuru16@gmail.com>
2023-08-10 14:19:39 -0400

    Adding React Components

---
+++ client-app/src/demo.ts
@@ -0,0 +1,21 @@
+export interface IDuck {
+    name: string;
+    numLegs: number;
+    makeSound: (sound: string) => void;
+}
+
+const duck1: IDuck = {
+    name: 'huey',
+    numLegs: 2,
+    makeSound: (sound: any) => console.log(sound)
+}
+
+const duck2: IDuck = {
+    name: 'dewey',
+    numLegs: 2,
+    makeSound: (sound: any) => console.log(sound)
+}
+
+duck1.makeSound('quack');
+
+export const ducks = [duck1, duck2]
