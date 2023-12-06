7cc05afcedfdc209aa537fa4d91a882d0d1b7a0d
majestronik <frknkuru16@gmail.com>
2023-12-06 14:50:19 -0500

    Security Policies Activate

--- API/Program.cs
+++ API/Program.cs
@@ -30,7 +30,7 @@ app.UseXContentTypeOptions();
 app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
-app.UseCspReportOnly(opt => opt
+app.UseCsp(opt => opt
     .BlockAllMixedContent()
     .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
     .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
